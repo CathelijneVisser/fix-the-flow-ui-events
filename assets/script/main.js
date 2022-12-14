102d8a4c7d5cb76dc0f7e5d49d3cc68ed1424019
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2022-12-14 16:44:30 +0100

    wip

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,4 +1,14 @@
 let interaction = document.querySelector("a:nth-of-type(12)");
+const rainbow = [
+	"red",
+	"orange",
+	"yellow",
+	"green",
+	"blue",
+	"purple",
+	"#050840",
+];
+let count = 0;
 
 interaction.addEventListener("click", jumpHandler);
 interaction.addEventListener("animationend", jumpHandler);
@@ -7,14 +17,16 @@ function jumpHandler() {
 	interaction.classList.toggle("jump");
 }
 
-let BFrontend = document.querySelector("a:first-child");
 let body = document.querySelector("body");
 let link = document.querySelectorAll("a");
+let section = document.querySelector("section");
+let BFrontend = document.querySelector("a:first-child");
 let BDesign = document.querySelector("a:nth-of-type(2)");
 let BEnTeken = document.querySelector("a:nth-of-type(3)");
 let BDevelepment = document.querySelector("a:nth-of-type(4)");
 let BSprint = document.querySelector("a:nth-of-type(5)");
-let section = document.querySelector("section");
+let BCode = document.querySelector("a:nth-of-type(20)");
+let BFix = document.querySelector("a:nth-of-type(6)");
 
 BFrontend.addEventListener("click", gradient);
 BDesign.addEventListener("click", revers);
@@ -23,6 +35,15 @@ BEnTeken.addEventListener("animationend", gravity);
 BDevelepment.addEventListener("click", imbignow);
 BDevelepment.addEventListener("animationend", imbignow);
 BSprint.addEventListener("mouseout", left);
+BCode.addEventListener("dblclick", dbjump);
+BCode.addEventListener("animationend", dbjump);
+BFix.addEventListener("touchmove", wiggle);
+BFix.addEventListener("animationend", wiggle);
+// window.addEventListener("load", vallen);
+// window.addEventListener("animationend", vallen);
+window.addEventListener("resize", shake);
+window.addEventListener("animationend", shake);
+window.addEventListener("keydown", rain);
 
 function gradient() {
 	body.classList.toggle("kleurtje");
@@ -45,4 +66,32 @@ function left() {
 	setTimeout(() => {
 		BSprint.classList.toggle("left");
 	}, 1000);
+}
+
+function dbjump() {
+	BCode.classList.toggle("dbjump");
+}
+
+function wiggle() {
+	BFix.classList.toggle("wiggle");
+}
+
+function vallen() {
+	section.classList.toggle("vallen");
+}
+
+function shake() {
+	section.classList.toggle("shake");
+}
+
+function rain() {
+	body.style.backgroundColor = rainbow[count];
+	count = count + 1;
+	if (count == rainbow.length) {
+		count = 0;
+	}
+
+	// Rainbow is de array waarin de kleuren staan
+	// count is de variable die optelt om naar de volgende kleur te gaan
+	// if stament is zodat count niet boven de kleuren heen komt
 }
